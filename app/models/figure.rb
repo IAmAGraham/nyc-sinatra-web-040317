94691188034bd14f4666d1dcb56db44c2481b4f7
github username <github email address>
2017-04-28 09:39:55 -0400

    5 failures left

--- app/models/figure.rb
+++ app/models/figure.rb
@@ -1,6 +1,7 @@
 class Figure <ActiveRecord::Base
-  has_many :title_figures
+
   has_many :landmarks
+  has_many :title_figures
   has_many :titles, through: :title_figures
 
 end
--- app/models/title_figure.rb
+++ app/models/title_figure.rb
@@ -1,6 +1,6 @@
 class TitleFigure < ActiveRecord::Base
 
-  belongs_to :title_id
-  belongs_to :figure_id
+  belongs_to :title
+  belongs_to :figure
 
 end
